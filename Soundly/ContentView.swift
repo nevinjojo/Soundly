d73cc41cc28ac673e06bbf73b27c4f58b156529a
Nevin Jojo <nevinjojo98@gmail.com>
2019-09-28 19:53:59 +1200

    Initial Commit

---
+++ Soundly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Soundly
+//
+//  Created by Nevin Jojo on 28/09/19.
+//  Copyright © 2019 Nevin Jojo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
